53fe60c4d87054a997d9d976471cf07883a5b2e5
Pedro Fraga <antoniopedrofraga@gmail.com>
2016-07-02 06:31:37 +0100

    add projects is missing

---
+++ assets/js/template.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($) {
+
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+	
+
+	var initio_parallax_animation = function() { 
+		$('.parallax').each( function(i, obj) {
+			if(!isMobile.any()) {
+   				var speed = $(this).attr('parallax-speed');
+			}
+			if( speed ) {
+				var background_pos = '-' + (window.pageYOffset / speed) + "px";
+				$(this).css( 'background-position', 'center ' + background_pos );
+			}
+		});
+	}
+	
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+		initio_parallax_animation();
+	});
+
+});
